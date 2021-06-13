495c54befb5d0c85b235a86de10138802a7ffc0f
tocchimittsu <arimagedon21@gmail.com>
2021-06-13 15:18:36 +0900

    修正

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         # Windowの初期設定を行う。
@@ -10,26 +9,42 @@ class Application(tk.Frame):
         # geometryについて : https://kuroro.blog/python/rozH3S2CYE0a0nB3s2QL/
         self.master.geometry("300x200")
 
-        # Window内にFrameを作成する。
+        # Windowを親要素をとして、frame Widget(Frame)を作成する。
         # Frameについて : https://kuroro.blog/python/P20XOidA5nh583fYRvxf/
-        tk.Frame(self.master)
+        frame = tk.Frame(self.master)
+        # Windowを親要素とした場合に、frame Widget(Frame)をどのように配置するのか?
+        frame.pack()
 
-        # ラベル1Widgetを作成する。
+        # frame Widget(Frame)を親要素として、label Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅設定
+        # height : 高さ設定
+        # bg : 背景色設定
+        # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
         # Labelについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
-        label1 = tk.Label(text="1",
-                          width=30, height=15, bg="orchid4")
-        # ラベル2Widgetを作成する。
+        label1 = tk.Label(frame, text="label1", width=30, height=15, bg="red")
+        # frame Widget(Frame)を親要素として、label Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅設定
+        # height : 高さ設定
+        # bg : 背景色設定
+        # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
         # Labelについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
-        label2 = tk.Label(text="2",
-                          width=30, height=15, bg="dark turquoise")
-        # ラベル3Widgetを作成する。
+        label2 = tk.Label(frame, text="label2", width=30, height=15, bg="green")
+        # frame Widget(Frame)を親要素として、label Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅設定
+        # height : 高さ設定
+        # bg : 背景色設定
+        # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
         # Labelについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
-        label3 = tk.Label(text="3",
-                          width=30, height=15, bg="MidnightBlue")
+        label3 = tk.Label(frame, text="label3", width=30, height=15, bg="blue")
 
-        # Windowを親要素とした場合に、それぞれラベルWidgetをどのように配置するのか?
+        # frame Widget(Frame)を親要素とした場合に、label1 Widgetをどのように配置するのか?
         label1.pack()
+        # frame Widget(Frame)を親要素とした場合に、label2 Widgetをどのように配置するのか?
         label2.pack()
+        # frame Widget(Frame)を親要素とした場合に、label3 Widgetをどのように配置するのか?
         label3.pack()
 
 
